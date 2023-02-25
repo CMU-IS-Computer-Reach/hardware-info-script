732db0f571f98e02ab4b8a99ff1f04e05f2b7bb9
hachiyuki8 <caladuil@outlook.com>
2023-02-25 15:45:42 -0500

    add better error handling

--- script.py
+++ script.py
@@ -2,36 +2,63 @@ import re
 import subprocess
 
 class EquipmentInfo():
-    def __init__(self):
-        self.model_name = ""          # CPU model, string
-        self.RAM = -1                 # RAM size (GB), int
-        self.screen_size = (-1, -1)   # Screen size (pixels), (int, int)
+    def __init__(self):               # description                     # type
+        self.model_name = ""          # CPU model                         string
+        self.RAM = -1                 # RAM size (GB)                     int
+        self.screen_size = (-1, -1)   # Screen size (pixels)              (int, int)
+        self.errors = dict()          # field -> errors during parsing    str -> str
         self.initialize()
 
     def initialize(self):
         # CPU model
-        output = subprocess.check_output("cat /proc/cpuinfo | grep 'model name'", 
-            shell = True,
-            text = True)
-        r = re.match(r"\s*model name\s*:\s*(.*)", output)
-        if r:
-            self.model_name = r.group(1)
+        try:
+            output = subprocess.check_output("cat /proc/cpuinfo | grep 'model name'", 
+                shell = True,
+                text = True,
+                stderr = subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            self.errors["model name"] = e.output if e.output else "`grep` didn't find match"
+        else:
+            try:
+                r = re.match(r"\s*model name\s*:\s*(.*)", output)
+                self.model_name = r.group(1)
+            except Expection as e:
+                self.errors["model name"] = str(e)
 
         # RAM size (GB)
-        output = subprocess.check_output("cat /proc/meminfo | numfmt --field 2 --from-unit=Ki --to-unit=Gi | sed 's/ kB/G/g' | grep 'MemTotal'",
-            shell = True,
-            text = True)
-        r = re.match(r"\s*MemTotal\s*:\s*(\d*)G", output)
-        if r:
-            self.RAM = int(r.group(1))
+        try:
+            output = subprocess.check_output("cat /proc/meminfo | numfmt --field 2 --from-unit=Ki --to-unit=Gi | sed 's/ kB/G/g' | grep 'MemTotal'",
+                shell = True,
+                text = True,
+                stderr = subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            self.errors["RAM"] = e.output if e.output else "`grep` didn't find match"
+        else:
+            try:
+                r = re.match(r"\s*MemTotal\s*:\s*(\d*)G", output)
+                self.RAM = int(r.group(1))
+            except Exception as e:
+                self.errors["RAM"] = str(e)
 
         # Screen size (pixels)
-        output = subprocess.check_output("xdpyinfo | grep 'dimensions'",
-            shell = True,
-            text = True)
-        r = re.match(r"\s*dimensions:\s*(\d*)x(\d*)\s*pixels.*", output)
-        if r:
-            self.screen_size = (int(r.group(1)), int(r.group(2)))
+        try:
+            output = subprocess.check_output("xdpyinfo | grep 'dimensions'",
+                shell = True,
+                text = True)
+        except subprocess.CalledProcessError as e:
+            self.errors["screen size"] = e.output
+        else:
+            try:
+                r = re.match(r"\s*dimensions:\s*(\d*)x(\d*)\s*pixels.*", output)
+                self.screen_size = (int(r.group(1)), int(r.group(2)))
+            except Exception as e:
+                self.errors["screen size"] = str(e)
+
+    def print_errors(self):
+        if len(self.errors):
+            print("****PARSING ERRORS****")
+        for field, err in self.errors.items():
+            print(f"-{field}: {err}")
 
     def __str__(self):
         return ("model name: %s\nRAM(GB): %s\nscreen size(pixels): %sx%s" % (
@@ -43,6 +70,7 @@ class EquipmentInfo():
 def main():
     info = EquipmentInfo()
     print(info)
+    info.print_errors()
 
 if __name__ == "__main__":
     main()
