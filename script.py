caf079d6df8b39e21d2dae7640649af7e5c313f0
Joanna Yao <xinyao@andrew.cmu.edu>
2023-03-05 23:12:49 -0500

    add battery health, reformat earlier parsing

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ class EquipmentInfo():
         self.model_name = ""          # CPU model                         string
         self.RAM = -1                 # RAM size (GB)                     int
         self.screen_size = (-1, -1)   # Screen size (pixels)              (int, int)
+        self.battery_health = -1      # Battery health (%)                float
         self.errors = dict()          # field -> errors during parsing    str -> str
         self.initialize()
 
@@ -27,7 +28,7 @@ class EquipmentInfo():
 
         # RAM size (GB)
         try:
-            output = subprocess.check_output("cat /proc/meminfo | numfmt --field 2 --from-unit=Ki --to-unit=Gi | sed 's/ kB/G/g' | grep 'MemTotal'",
+            output = subprocess.check_output("cat /proc/meminfo | numfmt --field 2 --from-unit=Ki --to-unit=Gi | sed 's/ kB/G/g' | grep 'MemTotal' | awk '{print $2}'",
                 shell = True,
                 text = True,
                 stderr = subprocess.STDOUT)
@@ -35,24 +36,39 @@ class EquipmentInfo():
             self.errors["RAM"] = e.output if e.output else "`grep` didn't find match"
         else:
             try:
-                r = re.match(r"\s*MemTotal\s*:\s*(\d*)G", output)
+                r = re.match(r"(\d*)G", output)
                 self.RAM = int(r.group(1))
             except Exception as e:
                 self.errors["RAM"] = str(e)
 
         # Screen size (pixels)
         try:
-            output = subprocess.check_output("xdpyinfo | grep 'dimensions'",
+            output = subprocess.check_output("xrandr --current | grep '*' | awk '{print $1}'",
                 shell = True,
                 text = True)
         except subprocess.CalledProcessError as e:
             self.errors["screen size"] = e.output
         else:
             try:
-                r = re.match(r"\s*dimensions:\s*(\d*)x(\d*)\s*pixels.*", output)
+                r = re.match(r"(\d*)x(\d*)", output)
                 self.screen_size = (int(r.group(1)), int(r.group(2)))
             except Exception as e:
                 self.errors["screen size"] = str(e)
+                
+        # Battery health (%) # TODO: confirm the exact number we want
+        try:
+            output = subprocess.check_output("upower -i `upower -e | grep 'BAT'` | grep 'capacity' | awk '{print $2}'",
+                shell = True,
+                text = True)
+        except subprocess.CalledProcessError as e:
+            self.errors["battery health"] = e.output
+        else:
+            try:
+                r = re.match(r"(\d\.*)%", output)
+                self.battery_health = float(r.group(1))
+            except Exception as e:
+                self.errors["battery health"] = str(e)
+        
 
     def print_errors(self):
         if len(self.errors):
@@ -61,11 +77,12 @@ class EquipmentInfo():
             print(f"-{field}: {err}")
 
     def __str__(self):
-        return ("model name: %s\nRAM(GB): %s\nscreen size(pixels): %sx%s" % (
+        return ("model name: %s\nRAM(GB): %s\nscreen size(pixels): %sx%s\nbattery health: %s\%" % (
                     self.model_name, 
                     self.RAM, 
                     self.screen_size[0],
-                    self.screen_size[1]))    
+                    self.screen_size[1],
+                    self.battery_health))    
 
 def main():
     info = EquipmentInfo()
